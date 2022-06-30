858c8f89f49cdce2562c76e277dda69ac1b564e8
Jason Prasad <jasongprasad@gmail.com>
2022-06-30 20:35:32 +0100

    Update tests with expectations

--- Tests/AlamofireTests/AlamofireTests.swift
+++ Tests/AlamofireTests/AlamofireTests.swift
@@ -1,6 +1,18 @@
 import Alamofire
-import XCTest
+import XCTVaporTestCase
 
-final class AlamofireTests: XCTestCase {
-    func testExample() throws {}
+final class AlamofireTests: XCTVaporTestCase {
+    func testGETHello() async throws {
+        let expect = expectation(description: "should GET /hello")
+        let url = appURL.appendingPathComponent("hello")
+
+        AF.request(url).response { response in
+            let decoded = response.data.map { String(data: $0, encoding: .utf8) }
+            XCTAssertEqual(response.response?.statusCode, 200)
+            XCTAssertEqual(decoded, "Hello, world!")
+            expect.fulfill()
+        }
+
+        wait(for: [expect], timeout: 1)
+    }
 }
--- Tests/URLSessionTests/URLSessionTests.swift
+++ Tests/URLSessionTests/URLSessionTests.swift
@@ -1,12 +1,14 @@
 import XCTVaporTestCase
 
 final class URLSessionTests: XCTVaporTestCase {
-    func testExample() async throws {
+    func testGETHello() async throws {
         let url = appURL.appendingPathComponent("hello")
         let request = URLRequest(url: url)
+
         let (data, response) = try await URLSession.shared.data(for: request)
         let decoded = String(data: data, encoding: .utf8)
         let httpURLResponse = response as? HTTPURLResponse
+
         XCTAssertEqual(decoded, "Hello, world!")
         XCTAssertEqual(httpURLResponse?.statusCode, 200)
     }
